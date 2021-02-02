027ca613551b43de82726ab74a046a039af97238
Python3 <pun_punyawatder@hotmail.com>
2021-02-02 15:52:19 +0700

    02112021

---
+++ Python for Beginner Phase1/35.py
@@ -0,0 +1,18 @@
+#สร้างขอบตาราง
+""" 
+xxxx
+x  x
+x  x
+xxxx
+"""
+
+last = int(input("input :"))
+for i in range(last):
+    for y in range(last):
+        if i == 0 or i==last-1 or y==0 or y==last-1:
+            print("x",end='')
+        #elif i %2!=0 or i %2==0:
+            #print("o",end='')
+        else:
+            print(" ",end='')
+    print(" ")
