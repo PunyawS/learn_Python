5e17347971f90be420eaec6b1058e10de4b8f6d6
Python3 <pun_punyawatder@hotmail.com>
2021-02-04 00:21:50 +0700

    04022021

---
+++ Python for Beginner Phase2/37 Start Phase2.py
@@ -0,0 +1,34 @@
+#Primitive&Non-Primitive
+
+#Primitive
+""" a,b,c,d,e = 1,2,3,4,5 """
+
+#Non-Primitive
+number=[1,2,3,4,5] # sub set list is 1-10
+""" #number=[] # List ว่าง
+#name=["Piwpiw","Arpo"] # sub set list is piwpiw,arpo
+#all=[10,"Piwpiw",True,1.11] """
+
+#construtor
+""" name=list([1,2,3,4,5,6,7,8,9,10]) # same name=["Piwpiw","Arpo"] """
+
+#การเข้าถึงข้อมูลและแสดงผล
+""" print(name)
+print(all)
+print(number[-1:])  """
+
+#การแก้ไขข้อมูล
+""" print("Before edit :",number)
+number[2]=9
+print("After edit :",number) """
+
+#Loop
+""" sum=0
+for x in number:
+    print("sub set :",x)
+    sum+=x
+print(sum) """
+
+#การตรวจสอบข้อมูลสมาชิก
+
+
