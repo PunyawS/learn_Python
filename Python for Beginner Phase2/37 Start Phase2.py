2e5ab50b97308b0ca615c6e87c967ca4d749db65
Python3 <pun_punyawatder@hotmail.com>
2021-02-04 19:08:12 +0700

    04022021

--- Python for Beginner Phase2/37 Start Phase2.py
+++ Python for Beginner Phase2/37 Start Phase2.py
@@ -30,5 +30,54 @@ for x in number:
 print(sum) """
 
 #การตรวจสอบข้อมูลสมาชิก
+""" if 4 in number:
+    print("เป็น")
+else:
+    print("ไม่เป็น") """
+
+#การนับจำนวนสมาชิก
+""" print(len(number)) """
+
+names=["KKK","AAA","CCC"]
+#Len ร่วมกับ Loop
+""" for i in range(len(names)):
+    print(names[i]) 
+for kk in names:
+    print(kk) """
+
+#append, insert
+""" print("Before :",names)
+names.append("BBB") #ต่อท้าย
+print("After :",names)
+names.insert(1,"JJJ") #แทรกกลาง
+print("After :",names) """
+
+#remove,pop,del,clear
+""" names.remove("AAA") #ตัวที่เลือก
+print("Remove :",names) 
+names.pop()         #ตัวท้ายสุด
+print("Pop :",names)
+del names[0]        #เลือกลบโดยบอก index
+print(names)
+names.clear()         #ลบสมาชิกออก
+print("Clear :",names) """
+
+#Copy data list A ==> B
+""" x=[]
+x=names.copy()
+print(x) """
+
+#รวมข้อมูลเพื่อทำ List ใหม่
+meowmeow = ["Piwpiw","Arpo"]
+""" allgroup=names+meowmeow
+print(allgroup) """
+#Extend รวมโดยใช้ List เดิม
+""" meowmeow.extend(number)
+print(meowmeow) """
 
 
+
+#แสดงจำนวนสมาชิกที่ซ้ำกัน count
+""" master=[1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,1,2,3,4,5,6,7]
+Y=master.count(4)
+print(Y) """
