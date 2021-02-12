c25211ef095144668531cea7b05700bb6d8d63b2
Python3 <pun_punyawatder@hotmail.com>
2021-02-12 23:08:42 +0700

    12022021

--- Python for Beginner Phase2/50. Dict.py
+++ Python for Beginner Phase2/50. Dict.py
@@ -8,9 +8,55 @@ print(type(y))
 #Dictionary => key(Index), value(value in index)
 
 # dict กำหนดชื่อ key ของ index ได้
-color={"blue":"สีน้ำเงิน","yello":"สีเหลือง"} #{key:value}
-colors=dict({"blue":"สีน้ำเงิน","yello":"สีเหลือง"})
-print(type(color))
+color={"blue":"สีน้ำเงิน","yellow":"สีเหลือง"} #{key:value}
+colors=dict({"blue":"สีน้ำเงิน","yellow":"สีเหลือง"})
+""" print(type(color))
 print(type(colors))
 print(color["blue"])
-print(colors["yello"])
+print(colors["yellow"]) """
+
+#Edit
+""" color["blue"]="น้ำเงิน" """
+#Add
+color.update({"green":"สีเขียว","orange":"สีส้ม"})
+""" print(color) """
+
+#for loop
+""" for x,y in color.items():
+    print(x,y)
+ """
+#การลบข้อมูลสมาชิกโดยใช้ pop
+""" color.pop("blue")
+color.popitem()
+color.clear()
+del color()
+print(color) """
+
+#copy
+""" x=color.copy()
+print(x) """
+
+#Dict in dict
+market={
+    "Starbuck":{
+        "name":"A",
+        "menu":["coffee","milk","green tea"],
+        "zone":"1"
+    },
+    "KFC":{
+        "name":"B",
+        "menu":["Chicken"],
+        "zone":"2"
+    },
+    "McDonald's":{
+        "name":"C",
+        "menu":["Burger","Chicken"],
+        "zone":"3"
+    },
+    "Pizza Hut":{
+        "name":"D",
+        "menu":["Pizza"],
+        "zone":"4"
+    }
+}
+print(market["KFC"]["menu"])
