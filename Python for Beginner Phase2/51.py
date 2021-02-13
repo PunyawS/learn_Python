a88b1246afe67d14163654fac1709b323975dccb
Python3 <pun_punyawatder@hotmail.com>
2021-02-13 18:10:36 +0700

    13022021

---
+++ Python for Beginner Phase2/51.py
@@ -0,0 +1,22 @@
+#Function
+#การสร้าง Function / เรียกใช้ function
+
+# def namefunction ():
+#       statement
+
+#โปรแกรมย่อย
+def sayhello():
+    print("hello Function")
+
+def sayhi():
+    print("hi Function")
+
+def add():
+    x=3+1
+    print(x)
+
+#โปรแกรมหลัก
+sayhello()
+add()
+""" for i in range(4):
+    add() """
