c42c7aa4559be63d1fd3ca2ccd3f85aac352cd3f
Python3 <pun_punyawatder@hotmail.com>
2021-02-21 22:50:50 +0700

    21022021

---
+++ Python for Beginner Phase3/93.py
@@ -0,0 +1,15 @@
+#Utility Module
+#Math Utility
+
+#ไม่ต้อง import math
+x=min(1,2,3,4,5,6,7,8,9,10) #หาเลขที่น้อยที่สุดในกลุ่ม
+print(x)
+
+y=max(1,2,3,4,5,6,7,8,9,10) #หาเลขที่มากที่สุดในกลุ่ม
+print(y)
+
+z=abs(-50) #absolute
+print(z)
+
+p=pow(5,2) #เลขยกกำลัง
+print(p)
---
+++ Python for Beginner Phase3/94.py
@@ -0,0 +1,26 @@
+import math
+
+x=math.sqrt(9) #square root
+print(x)
+
+score=math.floor(80.4) #ปัดเศษลง
+score1=math.ceil(80.4) #ปัดเศษขึ้น
+print(score)
+print(score1)
+print(math.pi) #เพลงค่า Pi
+
+#Assignment มุมตรีโกณมิติ Sic cos tan
+convert=math.radians(90) #radians(องศา Degree)
+print(convert)
+x=math.sin(convert) #หาค่า Sin(radians)
+print(x)
+
+#การหาระยะทางระหว่างจุดสองจุด (บนระบบพิกัดฉาก)
+point1=[2,-3]
+point2=[7,-3]
+d=math.dist(point1,point2)
+print(d)
+
+#การหาค่า log
+l1=math.log2(32)
+print(l1)
